e9e1ee91026b96a752f0ee6f23ba6d095dee4bbf
Jihyeok <saurus2@naver.com>
2017-03-22 00:25:15 +0900

    0321

---
+++ BOJ1541LostBrace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    char cmd;
+    int n,s,m=0;
+    for(scanf("%d",&s);scanf("%c",&cmd),cmd!=10;){
+        if(cmd=='-') m=1;
+        scanf("%d",&n);
+        if(m) s -= n;
+        else s += n;
+    }
+    printf("%d",s);
+    return 0;
+}
