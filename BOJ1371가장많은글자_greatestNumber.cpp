8d838eb9c15d79fb8032b1e5da1ba2c06733c16a
Jihyeok <saurus2@naver.com>
2017-04-20 20:36:14 +0900

    0420

--- BOJ1371가장많은글자_greatestNumber.cpp
+++ BOJ1371가장많은글자_greatestNumber.cpp
@@ -1,15 +1,15 @@
-nclude <iostream>
+#include <iostream>
 using namespace std;
 int main(){
-	int arr[130] = {0},i=0,maxv=-987654321;
+	int arr[26] = {0},i=0,maxv=-987654321;
 	char c;
 	while((c=getchar())&& c!=EOF){
 		islower(c);
 		i=c-'a';
 		arr[i]++;
-		if(maxv < arr[i] && c != ' ' && c != 10) {maxv = arr[i];}
+		if(maxv < arr[i] && i>-1 && i<=26) {maxv = arr[i];}
 	}
-	for(int i=0; i<=130; i++) if(arr[i] == maxv) cout << (char)(i+'a');
+	for(int i=0; i<=26; i++) if(arr[i] == maxv) cout << (char)(i+'a');
 	return 0;
 }
 
